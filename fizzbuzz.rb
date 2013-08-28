a6bb02a70a541e04412767778d0f52cc4e92d6de
Jonathan Tsang <chewymeister88@googlemail.com>
2013-08-28 15:59:22 +0100

    refactored final fizzbuzz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,67 +1,58 @@
 
 #########################################################
-#                  SIMPLER VERSION
-#########################################################
-# class Fizzbuzz
-
-#   def returns_buzzfizz_simple(number)
-
-
-
-#   end
-
-
-#   private
-
-#   # Algorythm
-
-
-
-
-# end
-
-
-
-
-
-
-#########################################################
 #                  FIRST VERSION
 #########################################################
 
 class Fizzbuzz
-	def returns_fizz(number)
-    divisible_by_3?(number) ? 'Fizz' : number
-  end
 
-  def returns_buzz(number)
-    divisible_by_5?(number) ? 'Buzz' : number
-  end
 
   def returns_fizzbuzz(number)
-    divisible_by_35?(number) ? 'FizzBuzz' : number
+    if divisible_by_15?(divisible_by_15?(number))
+    elsif divisible_by_5?(divisible_by_5?(number))
+    elsif divisible_by_3?(divisible_by_3?(number))
+    else number
+    end
   end
 
 
-# algorythms
 
-
+    def divisible_by_15?(number)
+    number % 15 == 0
+    return 'FizzBuzz'
+  end
 
   def divisible_by_3?(number)
     number % 3 == 0
+    return 'Fizz'
   end
 
-  def divisible_by_5?(number)
+    def divisible_by_5?(number)
     number % 5 == 0
+    return 'Buzz'
   end
 
-  def divisible_by_35?(number)
-    number % 5 == 0 && number % 3 == 0
+
+
+
+
+  # def returns_fizzbuzz(number)
+  #   if number % 15 == 0
+  #     'FizzBuzz'
+  #   elsif number % 5 == 0
+  #     'Buzz'
+  #   elsif number % 3 == 0
+  #     'Fizz'
+  #   else
+  #     number
+  #   end
+  # end
+
+  def print_up_to_100
+    100.times do |i|
+      puts returns_fizzbuzz(i+1)
+    end
   end
 
-end
-
-class Fizzfuck
 end
 
 
