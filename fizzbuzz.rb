12258f8d59445d3696d9f19e5f55d1945302c150
Jonathan Tsang <chewymeister88@googlemail.com>
2013-09-18 15:22:05 +0100

    refactored version

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,51 +5,32 @@
 
 class Fizzbuzz
 
-
-  def returns_fizzbuzz(number)
-    if divisible_by_15?(divisible_by_15?(number))
-    elsif divisible_by_5?(divisible_by_5?(number))
-    elsif divisible_by_3?(divisible_by_3?(number))
+  def value_for_fizzbuzz(number)
+    if divisible_by_15?(number)
+      return 'FizzBuzz'
+    elsif divisible_by_5?(number)
+      return 'Fizz'
+    elsif divisible_by_3?(number)
+      return 'Buzz'
     else number
     end
   end
 
-
-
-    def divisible_by_15?(number)
+  def divisible_by_15?(number)
     number % 15 == 0
-    return 'FizzBuzz'
   end
 
   def divisible_by_3?(number)
     number % 3 == 0
-    return 'Fizz'
   end
 
     def divisible_by_5?(number)
     number % 5 == 0
-    return 'Buzz'
   end
-
-
-
-
-
-  # def returns_fizzbuzz(number)
-  #   if number % 15 == 0
-  #     'FizzBuzz'
-  #   elsif number % 5 == 0
-  #     'Buzz'
-  #   elsif number % 3 == 0
-  #     'Fizz'
-  #   else
-  #     number
-  #   end
-  # end
 
   def print_up_to_100
     100.times do |i|
-      puts returns_fizzbuzz(i+1)
+      puts value_for_fizzbuzz(i+1)
     end
   end
 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,38 +5,35 @@
 
 
 require_relative '../fizzbuzz'
- describe Fizzbuzz do
+describe Fizzbuzz do
 
-   it 'should return Fizz if divisible by 3' do
- 		 fizzbuzz = Fizzbuzz.new
-     fizzbuzz.returns_fizzbuzz(3).should eq 'Fizz'
-   end
- 
-   it 'should return Buzz if divisible by 5' do
-     fizzbuzz = Fizzbuzz.new
-     fizzbuzz.returns_fizzbuzz(5).should eq 'Buzz'
-     fizzbuzz.returns_fizzbuzz(10).should eq 'Buzz'
+	it 'should return Fizz if divisible by 3' do
+		 fizzbuzz = Fizzbuzz.new
+	 fizzbuzz.value_for_fizzbuzz(3).should eq 'Fizz'
+	end
 
-   end
+	it 'should return Buzz if divisible by 5' do
+	 fizzbuzz = Fizzbuzz.new
+	 fizzbuzz.value_for_fizzbuzz(5).should eq 'Buzz'
+	 fizzbuzz.value_for_fizzbuzz(10).should eq 'Buzz'
 
-   it 'should return FizzBuzz if divisible by 5 and 3' do
-     fizzbuzz = Fizzbuzz.new
-     fizzbuzz.returns_fizzbuzz(15).should eq 'FizzBuzz'
-     fizzbuzz.returns_fizzbuzz(60).should eq 'FizzBuzz'
-     fizzbuzz.returns_fizzbuzz(30).should eq 'FizzBuzz'
-   end
+	end
 
-   it 'should return number if not divisible by 5 and 3' do
-     fizzbuzz = Fizzbuzz.new
-     fizzbuzz.returns_fizzbuzz(11).should eq (11)
-     fizzbuzz.returns_fizzbuzz(13).should eq (13)
-     fizzbuzz.returns_fizzbuzz(47).should eq (47)
-   end
+	it 'should return FizzBuzz if divisible by 5 and 3' do
+	 fizzbuzz = Fizzbuzz.new
+	 fizzbuzz.value_for_fizzbuzz(15).should eq 'FizzBuzz'
+	 fizzbuzz.value_for_fizzbuzz(60).should eq 'FizzBuzz'
+	 fizzbuzz.value_for_fizzbuzz(30).should eq 'FizzBuzz'
+	end
 
+	it 'should return number if not divisible by 5 and 3' do
+	 fizzbuzz = Fizzbuzz.new
+	 fizzbuzz.value_for_fizzbuzz(11).should eq (11)
+	 fizzbuzz.value_for_fizzbuzz(13).should eq (13)
+	 fizzbuzz.value_for_fizzbuzz(47).should eq (47)
+	end
 
-
-
- end
+end
 
 
    # it 'should not access the private methods' do
